533beb549ab1a63b14dee30c5177bf009e7596b8
Gavin Radue <gavin.radue@payfast.co.za>
2024-05-24 12:54:00 +0200

    main: adding deploy script and script shortcut

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+echo '
+s
+gavinradue.co.za' > CNAME
+
+git init
+
+git add -A
+
+git commit -m 'deploy'
+
+git push -f git@github.com:gavinradue95/remembering-elena.git main:gh-pages
+
+cd -
