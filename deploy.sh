2c48737487da801b78b0b7cec45c25307726c506
Gavin Radue <gavin.radue@payfast.co.za>
2024-05-31 01:42:19 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
-
-set -e
-
 npm run build
 
-cd dist
+git commit -m 'deploy'
+
+git push origin main
+
+cd dist || exit
 
 echo 'gavinradue.co.za' > CNAME
+
+cp index.html 404.html
 
 git init
 
@@ -14,6 +17,6 @@ git add -A
 
 git commit -m 'deploy'
 
-git push -f git@github.com:gavinradue95/remembering-elena.git main:gh-pages
+git push
 
-cd -
+cd - || exit
